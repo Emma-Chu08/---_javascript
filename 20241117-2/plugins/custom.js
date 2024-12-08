17f794a8f0e09d2cb61c6991623780d73d3021fb
emma <emma90706@gmail.com>
2024-12-08 16:35:16 +0800

    20241208

--- 20241117-2/plugins/custom.js
+++ 20241117-2/plugins/custom.js
@@ -2,3 +2,21 @@ onload = () => {
   var grid = document.querySelector('#lokiPark article.row');
   var msnry = new Masonry(grid, { percentPosition: 'true' });
 }
+
+//cookie 設定
+//1.取得cookie
+const lokiCookieAry = document.cookie.split(';'),
+nodeCookie = document.querySelector('#lokiCookie'),
+keywordCookie = 'cookieUsed-agree';
+
+//2.檢查cookieAry是否為關鍵字
+if(aryCookie.includes(keywordCookie)){
+
+}else{
+
+}
+nodeCookie.style.display = 'block';
+nodeCookie.computedStyleMap('button').onclik = () =>{
+  document.cookie = keywordCookie;
+};
+
